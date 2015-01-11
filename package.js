36abbce2d8ae45a79b2ad2e0a61cae7ce53fcb1e
Yasar icli <hello@yasaricli.com>
2015-01-11 08:00:50 -0500

    version update

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'yasaricli:lxc',
     summary: 'LXC Linux Containers Interface for meteor',
-    version: '0.0.1',
+    version: '0.0.2',
     git: 'https://github.com/yasaricli/meteor-lxc.git'
 });
 
