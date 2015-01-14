14994e1c1cdbfc2f064e5db2c61e8aa0d68a84d9
Yaşar içli <hello@yasaricli.com>
2015-01-14 07:02:57 -0500

    outline fixed and ls removed.

--- lxc.js
+++ lxc.js
@@ -3,20 +3,37 @@
         Future = Npm.require('fibers/future'),
         Child = Npm.require('child_process'),
         ShortCuts = {
-            run: function(command, callback) { 
+            run: function(command, out) { 
                 var fut = new Future(),
                     C = Child.exec(command, function(err, data) {
-                        callback && callback(data);
+                        if (out) {
+                            fut.return({
+                                complete: function(callback) {
+                                    var Info = {};
+                                    ShortCuts.outputEach(data, function(d) {
+                                        var list = d.split(':'),
+                                            key = list[0],
+                                            val = list[1];
+                                        if (!Info[key] && val) {
+                                            Info[key.toLowerCase()] = val.trim();
+                                        }
+                                    });
+                                    callback(Info);
+                                }
+                            });
+                        }
                     });
 
                 // exit
                 C.on('exit', function(err) {
-                    fut.return({
-                        complete: function(callback) {
-                            var match = command.match(/-n (.*)/g);
-                            callback(!!err, match[0].split(' ')[1]);
-                        }
-                    });
+                    if (!out) {
+                        fut.return({
+                            complete: function(callback) {
+                                var match = command.match(/-n (.*)/g);
+                                callback(!!err, match[0].split(' ')[1]);
+                            }
+                        });
+                    }
                 });
                 return fut.wait();
             },
@@ -41,42 +58,11 @@
         stop: function(name) {
             return ShortCuts.run('lxc-stop -n ' + name);
         },
-        info: function(name, complete) {
-            ShortCuts.run('lxc-info -n ' + name, function(out) {
-                var Info= {};
-                ShortCuts.outputEach(out, function(d) {
-                    var list = d.split(':'),
-                        key = list[0],
-                        val = list[1];
-                    if (!Info[key] && val) {
-                        Info[key.toLowerCase()] = val.trim();
-                    }
-                });
-                complete && complete(Info);
-            });
-        },
         clone: function(name, newname) {
             return ShortCuts.run('lxc-clone -o '+ name +' -n ' + newname);
         },
-        ls: function(complete) {
-            ShortCuts.run('lxc-ls --fancy', function(out) {
-                var result = { RUNNING: [], STOPPED: [] };
-                ShortCuts.outputEach(out, function(d) {
-                    var list = _.compact(d.split(' ')),
-                        islabel = list[0] == 'NAME' || list.length === 1;
-                    if (!islabel && list.length) {
-                        // NAME, STATE, IPV4, IPV6, AUTOSTART
-                        result[list[1]].push({
-                            name: list[0],
-                            state: list[1],
-                            ipv4: list[2],
-                            ipv6: list[3],
-                            autostart: list[4]
-                        });
-                    }
-                }); 
-                complete(result);
-            });
+        info: function(name) {
+            return ShortCuts.run('lxc-info -n ' + name, true);
         }
     };
 }).call(this);
