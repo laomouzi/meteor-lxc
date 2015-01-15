dba8b52fc290d2406b3aef4adfc0340e1bf6a3d2
Yasar içli <hello@yasaricli.com>
2015-01-15 07:25:09 -0500

    slugify update version

--- package.js
+++ package.js
@@ -7,6 +7,6 @@ Package.describe({
 
 Package.onUse(function(api) {
     api.versionsFrom('1.0.2.1');
-    api.use(['underscore', 'yasaricli:slugify'], 'server');
+    api.use(['underscore', 'yasaricli:slugify@0.0.5'], 'server');
     api.addFiles('lxc.js', 'server');
 });
